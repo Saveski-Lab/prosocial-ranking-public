23aa3b2380a6ec6c79f602489d3a76e3ff588805
Soham De <soham.de2001@gmail.com>
2024-04-15 18:37:48 -0700

    add test code

---
+++ test_data.py
@@ -0,0 +1,113 @@
+# Sample data containing multiple text items
+
+BASIC_EXAMPLE = {
+    "session": {
+        "user_id": "193a9e01-8849-4e1f-a42a-a859fa7f2ad3",
+        "user_name_hash": "6511c5688bbb87798128695a283411a26da532df06e6e931a53416e379ddda0e",
+        "current_time": "2024-01-20 18:41:20",
+    },
+    "items": [
+        {
+        "id": "1",
+        "title": "From POTUS",
+        "text": "I just met with my national security team for an update on Iran’s attacks against Israel. Our commitment to Israel’s security against threats from Iran and its proxies is ironclad.",
+        "author_name_hash": "author1_hash",
+        "type": "post",
+        "platform": "twitter",
+        "created_at": "2024-04-12 10:15:00",
+        "engagements": {"likes": 50, "retweets": 15}
+    },
+    {
+        "id": "2",
+        "title": "From POTUS",
+        "text": "Tune in as @KamalaHarris delivers remarks in Arizona.",
+        "author_name_hash": "author2_hash",
+        "type": "post",
+        "platform": "facebook",
+        "created_at": "2024-04-14 14:30:00",
+        "engagements": {"likes": 100, "comments": 20}
+    },
+    {
+        "id": "3",
+        "title": "From angry voter",
+        "text": "The Supreme Court sent it back to the states and now YOU the voter get to decide on abortion. Trump says he wants YOU to decide. Biden is saying that HE SHOULD DECIDE and force abortion up to the moment of birth into all 50 states.",
+        "author_name_hash": "author3_hash",
+        "type": "post",
+        "platform": "reddit",
+        "created_at": "2024-04-16 16:45:00",
+        "engagements": {"upvotes": 30, "downvotes": 5}
+    },
+    {
+        "id": "4",
+        "title": "Tech Talk",
+        "text": "Learn about the latest advancements in technology at our upcoming tech talk event.",
+        "author_name_hash": "author4_hash",
+        "type": "post",
+        "platform": "twitter",
+        "created_at": "2024-04-18 18:00:00",
+        "engagements": {"likes": 80, "retweets": 18}
+    },
+    {
+        "id": "5",
+        "title": "Local Charity Fundraiser",
+        "text": "Support local charities and organizations at our community fundraiser event.",
+        "author_name_hash": "author5_hash",
+        "type": "post",
+        "platform": "facebook",
+        "created_at": "2024-04-20 20:15:00",
+        "engagements": {"likes": 120, "comments": 25}
+    },
+    {
+        "id": "6",
+        "title": "Discussion Panel on Environmental Sustainability",
+        "text": "Join us for a discussion panel on environmental sustainability and learn how you can make a positive impact.",
+        "author_name_hash": "author6_hash",
+        "type": "post",
+        "platform": "reddit",
+        "created_at": "2024-04-22 22:30:00",
+        "engagements": {"upvotes": 40, "downvotes": 8}
+    },
+    {
+        "id": "7",
+        "title": "Community Movie Night",
+        "text": "Enjoy a movie night under the stars with friends and family in our community park.",
+        "author_name_hash": "author7_hash",
+        "type": "post",
+        "platform": "twitter",
+        "created_at": "2024-04-24 19:45:00",
+        "engagements": {"likes": 75, "retweets": 20}
+    },
+    {
+        "id": "8",
+        "title": "Local Art Exhibition",
+        "text": "Discover local artists and their works at our community art exhibition.",
+        "author_name_hash": "author8_hash",
+        "type": "post",
+        "platform": "facebook",
+        "created_at": "2024-04-26 18:00:00",
+        "engagements": {"likes": 110, "comments": 30}
+    },
+    {
+        "id": "9",
+        "title": "Angry voter",
+        "text": "Trump lied too much.",
+        "author_name_hash": "author9_hash",
+        "type": "post",
+        "platform": "reddit",
+        "created_at": "2024-04-28 12:30:00",
+        "engagements": {"upvotes": 65, "downvotes": 10}
+    },
+    {
+        "id": "10",
+        "title": "Local Planting Day",
+        "text": "Help beautify our community by participating in our local planting day event.",
+        "author_name_hash": "author10_hash",
+        "type": "post",
+        "platform": "twitter",
+        "created_at": "2024-04-30 14:45:00",
+        "engagements": {"likes": 90, "retweets": 25}
+    }
+        
+    ],
+}
+
